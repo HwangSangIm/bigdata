257062e2f1adb271cf7953b3185ca245e0daa95b
Apples1243 <Apples1243@naver.com>
2025-11-07 14:11:44 +0900

    main update

--- 1.py
+++ 1.py
@@ -142,13 +142,20 @@ def resultset (fig=None):
             
     with col_next:
         # 그래프 출력 시에도 현재의 dfrs(==df_current)를 다음 분석에 적용하는 것은 가능
-        if st.button('다음 분석에 결과 적용', key=f'btn_next_{"graph" if fig else "data"}'): # 키 변경
-            st.session_state['df_current'] = dfrs
-            st.success('결과가 저장되어 다음 분석에 사용됩니다.')
-            time.sleep(3)
-            st.session_state.analysis_step_key += 1
-            st.rerun()
-            
+        if fig is None:
+            if st.button('다음 분석에 결과 적용', key=f'btn_next_{"graph" if fig else "data"}'): # 키 변경
+                st.session_state['df_current'] = dfrs
+                st.success('결과가 저장되어 다음 분석에 사용됩니다.')
+                time.sleep(3)
+                st.session_state.analysis_step_key += 1
+                st.rerun()
+        else:
+            if st.button('그래프 생성 데이터 적용', key=f'btn_next_{"graph" if fig else "data"}'): # 키 변경
+                st.session_state['df_current'] = dfrs
+                st.success('결과가 저장되어 다음 분석에 사용됩니다.')
+                time.sleep(3)
+                st.session_state.analysis_step_key += 1
+                st.rerun()
     with col_save:
         name = st.text_input("저장할 파일 이름을 적으세요.", key=f'save_input_{"graph" if fig else "data"}') # 키 변경
         
@@ -273,27 +280,24 @@ if st.session_state['df_current'] is not None:
         second_option = ['선택하세요','조건에 맞는 데이터만 추출하기','필요한 변수만 추출하기','순서대로 정렬하기','집단별로 요약하기']
         secondselect = st.selectbox('어떠한 방법으로 전처리 하시겠습니까?',second_option,key=f'second_select_{st.session_state.analysis_step_key}')
         selected_index = second_option.index(secondselect)
-        print(selected_index)
         if selected_index == 1:
+            query_oper = st.radio("선택한 조건들을 어떻게 연결하시겠습니까?",('AND','OR'), index=0)
             thirdselect = st.multiselect('어떤 데이터를 추출하시겠습니까?',df.columns)
-            print(thirdselect)
             if len(thirdselect) > 0:
                 query_conditions = []
                 for index , columns_name in enumerate(thirdselect):
                     fourtyselect = st.selectbox(f'{columns_name}의 어떤 값을 추출하실겁니까?',df[columns_name].unique().tolist())
                     if df[columns_name].dtype == object or pd.api.types.is_string_dtype(df[columns_name]):
-                        condition = f'{columns_name} == "{fourtyselect}"'
+                        condition = f'`{columns_name}` == "{fourtyselect}"'
                     else:
-                        condition = f'{columns_name} == {fourtyselect}'
-                    print("hello" + condition)
+                        condition = f'`{columns_name}` == {fourtyselect}'
                     query_conditions.append(condition)
-                rs = ' or '.join(query_conditions)
+                rs = f'{query_oper.lower()}'.join(query_conditions)
                 dfrs = df.query(rs)
                 st.text('쿼리 결과')
                 resultset()
 
         elif selected_index == 2:
-            st.text('필요한 변수만 추출 및 제거')
             thirdselect = st.selectbox('추출하시겠습니까? 제거하시겠습니까?',['선택하세요','추출하기','제거하기'])
             if thirdselect == '추출하기':
                 fourtyselect = st.multiselect('추출할 변수를 선택하세요.',df.columns)
@@ -310,7 +314,6 @@ if st.session_state['df_current'] is not None:
                     st.text('제거한 결과')
                     resultset()
         elif selected_index == 3:
-            st.text('순서대로 정렬하기')
             thirdselect = st.selectbox('기준을 고르세요.',['선택하세요']+list(df.columns))
             if thirdselect != '선택하세요':
                 fourtyselect = st.checkbox('정렬 순서를 선택하세요. (기본값: 오름차순)')
@@ -323,7 +326,6 @@ if st.session_state['df_current'] is not None:
                     dfrs = df.sort_values(thirdselect)
                     resultset()
         elif selected_index == 4:
-            st.text('집단별로 요약하기')
             thirdselect = st.selectbox('기준을 고르세요.',['선택하세요'] + list(df.columns))
             if thirdselect != '선택하세요':
                 agg_text = st.text_input('추가할 변수명을 적으세요.')
@@ -342,7 +344,6 @@ if st.session_state['df_current'] is not None:
                                 dfrs = df.groupby(thirdselect , as_index = False).agg(**{agg_text : (fourtyselect , fiftyselect)}).sort_values(agg_text)
                                 resultset()
     if firstselect == '결측치 제어하기':
-        st.text('결측치 제어하기')
         second_option = ['선택하세요','결측치 확인','결측치 제거','결측치 변경']
         secondselect = st.selectbox('어떠한 방법을 사용하시겠습니까?',second_option)
         selected_index = second_option.index(secondselect)
@@ -390,7 +391,6 @@ if st.session_state['df_current'] is not None:
                     st.text('결측치의 내용을 변경한 결과')
                     resultset()
     if firstselect == '변수 추가하기':
-        st.text('변수 추가하기')
         secondselect = st.selectbox('어떠한 변수를 추가하실건가요?',['선택하세요','상수 추가','기존 열을 이용한 계산','사용자 입력'])
         if secondselect == '상수 추가':
             byunsuName = st.text_input('변수 이름을 입력하세요')
@@ -505,11 +505,9 @@ if st.session_state['df_current'] is not None:
                     except Exception as e:
                         st.error("코드 실행 중 오류가 발생했습니다.")
     if firstselect == '그래프로 출력하기':
-        st.text('그래프로 출력하기')
         fig , ax = plt.subplots()
         secondselect = st.selectbox('어떤 그래프를 그리겠습니까?',['선택하세요','산점도로 표현하기','막대 그래프로 표현하기','선 그래프로 표현하기','상자 그림으로 표현하기'])
         if secondselect == '산점도로 표현하기':
-            st.text('산점도로 표현하기')
             x_options = ['선택하세요'] + list(df.select_dtypes(include=np.number).columns)
             y_options = ['선택하세요'] + list(df.select_dtypes(include=np.number).columns)
             fo = st.selectbox('x축에 넣을 데이터를 고르세요.',x_options)
@@ -532,7 +530,6 @@ if st.session_state['df_current'] is not None:
             else:
                 plt.close(fig)
         elif secondselect == '막대 그래프로 표현하기':
-            st.text('막대 그래프로 표현하기')
             x_options = ['선택하세요'] + list(df.select_dtypes(include=['object','category']).columns)
             y_options = ['선택하세요'] + list(df.select_dtypes(include=np.number).columns)
             fo = st.selectbox('x축에 넣을 데이터를 고르세요.',x_options)
@@ -555,7 +552,6 @@ if st.session_state['df_current'] is not None:
             else:
                 plt.close(fig)
         if secondselect =='선 그래프로 표현하기':
-            st.text('선 그래프로 표현하기')
             x_options = ['선택하세요'] + list(df.select_dtypes(include=[np.number,'datetime']).columns)
             y_options = ['선택하세요'] + list(df.select_dtypes(include=np.number).columns)
             fo = st.selectbox('x축에 넣을 데이터를 고르세요.',x_options)
@@ -578,7 +574,6 @@ if st.session_state['df_current'] is not None:
             else:
                 plt.close(fig)
         if secondselect =='상자 그림으로 표현하기':
-            st.text('상자 그림으로 표현하기')
             x_options = ['선택하세요'] + list(df.select_dtypes(include=['object','category']).columns)
             y_options = ['선택하세요'] + list(df.select_dtypes(include=np.number).columns)
             fo = st.selectbox('x축에 넣을 데이터를 고르세요.',x_options)
